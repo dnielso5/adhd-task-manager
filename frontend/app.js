f943e0f35040cca3ed50b4a5fb460c0a611780d1
dnielso5 <107376351+dnielso5@users.noreply.github.com>
2025-07-31 13:15:06 -0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const api = "http://localhost:8009"
+const api = `${window.location.protocol}//${window.location.hostname}:8009`
 
 function addTask() {
   const title = document.getElementById("task-title").value
